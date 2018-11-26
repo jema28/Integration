b31a1708ab9d80e316dd8a9d3acc28ea66802302
Matt Davis <matthewpeterdavis@gmail.com>
2018-11-26 15:39:14 +0200

    fix linting issues

--- romanizer.js
+++ romanizer.js
@@ -1,31 +1,31 @@
 function romanizer(num) {
-switch (num) {
-    case 1: return 'I'; break;
-    case 2: return 'II'; break;
-    case 3: return 'III'; break;
-    case 4: return 'IV'; break;
-    case 5: return 'V'; break;
-    case 6: return 'VI' ;break;
-    case 7: return 'VII'; break;
-    case 8: return 'VIII'; break;
-    case 9: return 'IX'; break;
-    case 10: return 'X'; break;
-}
+    switch (num) {
+        case 1: return 'I';
+        case 2: return 'II';
+        case 3: return 'III';
+        case 4: return 'IV';
+        case 5: return 'V';
+        case 6: return 'VI';
+        case 7: return 'VII';
+        case 8: return 'VIII';
+        case 9: return 'IX';
+        case 10: return 'X';
+    }
 
-// var i=0;
-// if (num==4) {
-//   return result+=character;
-// }
-// i++;
-// if (num==4) {
-//       return character+'V';
-//   }
-//   else if (num==5) {
-//       return 'V';
-//   }
-//   else
-//   for (var i = 6; i<9; i++) {
-//       return 'V'+character;
-//   }
+    // var i=0;
+    // if (num==4) {
+    //   return result+=character;
+    // }
+    // i++;
+    // if (num==4) {
+    //       return character+'V';
+    //   }
+    //   else if (num==5) {
+    //       return 'V';
+    //   }
+    //   else
+    //   for (var i = 6; i<9; i++) {
+    //       return 'V'+character;
+    //   }
 }
 module.exports = romanizer;
